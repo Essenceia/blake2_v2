fccbace81d833f57945d3a2704e57aa4ffca5262
Julia Desmazes <julia.desmazes@gmail.com>
2025-10-17 12:24:02 -0500

    adding error module to detect basic errors during emulation, investigatting synth issue

--- fpga/emulator.v
+++ fpga/emulator.v
@@ -1,37 +1,43 @@
+`default_nettype none
 module emulator(
 	input wire clk,
-	input switch0_i,
-	input switch1_i,
+
+	input wire switch0_i,
+	input wire switch1_i,
+
 
 	input  wire [7:0] pmod_i,  
 	inout  wire [2:0] vga_i, 
 	inout  wire [10:3] vga_o, 
 	
+	output wire       led0_o, // error led
 	output wire [6:0] segA_o,
 	output wire [6:0] segB_o
 );
-
-assign segA_o = {6'h0, switch0_i};
-assign segB_o = 7'h00;
-
 wire rst_n, ena;
-wire [7:0] ui_in, uo_out, uio_in, uio_out, uio_oe;
+wire error; 
+wire [7:0] ui_in;
+wire [7:0] hash; 
+wire [7:0] uio_in; 
+wire [7:0] uio_out;
+wire [7:0]  uio_oe;
 
-assign rst_n = ~switch0_i;
+assign rst_n = (switch0_i == 1'b0);
 assign ena = switch1_i;
 assign ui_in = pmod_i;
-assign segB_o =  uo_out[6:0];
-assign segA_o = { 4'b0 , uo_out[7]};
-//assign vga_o[10:8] = 3'd0;
-assign vga_o[7] = uio_out[7] & uio_oe[7];
-//assign vga_o[6:4] = 3'd0;
-assign vga_o[3] = uio_out[3] & uio_oe[3];
+assign segB_o =  hash[6:0];
+assign segA_o = { 6'b0 , hash[7]};
+
+assign vga_o = { 3'd0,  // 10:8
+				uio_out[7] & uio_oe[7], // 7
+				3'd0, // 6:4
+				uio_out[3] & uio_oe[3]}; // 3
 assign uio_in[7:3] = 5'd0;
 assign uio_in[2:0] = vga_i[2:0] & ~uio_oe[2:0];
 
 top m_top(
 	.ui_in(ui_in),
-	.uo_out(),
+	.uo_out(hash),
 	.uio_in(uio_in),
 	.uio_out(uio_out),
 	.uio_oe(uio_oe),
@@ -40,5 +46,15 @@ top m_top(
 	.rst_n(rst_n)
 );
 
+// error 
+error m_err(
+	.clk(clk), 
+	.nreset(rst_n), 
+	
+	.uio_oe(uio_oe),
+	.uio_out(uio_out),
 
+	.error_o(error)
+);
+assign led0_o = error;
 endmodule
---
+++ fpga/error.v
@@ -0,0 +1,27 @@
+/* error detection module */ 
+module error(
+	input clk, 
+	input nreset,
+
+	input [7:0] uio_oe, 
+	input [7:0] uio_out,
+
+	output error_o
+);
+reg  err_q;
+wire err_next;
+wire switch_conf_err; 
+wire unexpected_out; 
+
+assign switch_conf_err = ~(uio_oe == 8'b10001000);
+assign unexpected_out = |(~uio_oe & uio_out);
+assign err_next = switch_conf_err | unexpected_out; 
+
+always @(posedge clk)
+	if (~nreset) 
+		err_q <= 1'b0;
+	else
+		err_q <= err_q | err_next; // sticky error bit
+
+assign error_o = err_q; 
+endmodule
