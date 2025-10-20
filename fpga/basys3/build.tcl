06f5d93e9da1fe7e7a00ccb274fe3dc8c3d7330f
Julia Desmazes - ws <julia.desmazes@gmail.com>
2025-10-20 13:54:12 -0700

    rework build flow, remove file include from build, move to setup to make calling synth via tcl cmd line less of a hastle

--- fpga/basys3/build.tcl
+++ fpga/basys3/build.tcl
@@ -1,17 +1,8 @@
 set project_path [lindex $argv 0]
-set utils_path [lindex $argv 1]
-set src_path [lindex $argv 2]
-set checkpoint_path [lindex $argv 3]
-puts "Implementation script called with project path $project_path and src path $src_path, generating checkpoint at $checkpoint_path"
+set checkpoint_path [lindex $argv 1]
+puts "Implementation script called with project path $project_path, generating checkpoint at $checkpoint_path"
 
 open_project $project_path 
-
-# load src
-read_verilog [glob *.v]
-read_verilog [glob -directory $src_path *.v]
-read_verilog [glob -directory $utils_path *.v]
-read_xdc [glob *.xdc]
-
 
 # synth
 synth_design -top emulator
--- fpga/basys3/setup.tcl
+++ fpga/basys3/setup.tcl
@@ -1,8 +1,16 @@
 set project_dir [lindex $argv 0]
 set project_name [lindex $argv 1]
+set utils_path [lindex $argv 2]
+set src_path [lindex $argv 3]
 
 puts "Creating project $project_name at path [pwd]/$project_dir"
 create_project -part xc7a35ticpg236-1L -force $project_name $project_dir
+
+# load src
+read_verilog [glob *.v]
+read_verilog [glob -directory $src_path *.v]
+read_verilog [glob -directory $utils_path *.v]
+read_xdc [glob *.xdc]
 
 close_project
 exit 0
