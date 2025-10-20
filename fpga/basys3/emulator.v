2ad46775fc0ee9df56f3a014ec39d4f785cbf3e7
Julia Desmazes - ws <julia.desmazes@gmail.com>
2025-10-20 14:15:46 -0700

    add correct io buffering to emulator IO, make it explicit

--- fpga/basys3/emulator.v
+++ fpga/basys3/emulator.v
@@ -1,37 +1,72 @@
 `default_nettype none
-module emulator(
+module emulator #(
+	parameter SWITCH_W = 2,
+	parameter PMOD_W = 8,
+	parameter LED_W = 1
+)
+(
 	input wire clk,
 
-	input wire [1:0] switch_i,
+	input wire [SWITCH_W:0] switch_i,
 
-	input  wire [7:0]  pmodA_i,  
-	inout  wire [7:0]  pmodB_io,  
-	output wire [7:0]  pmodC_o,  
+	input  wire [PMOD_W-1:0]  pmodA_i,  
+	inout  wire [PMOD_W-1:0]  pmodB_io,  
+	output wire [PMOD_W-1:0]  pmodC_o,  
  
-	output wire led_o
+	output wire [LED_W-1:0] led_o
 );
+localparam [7:0] DISSABLE_IO_PIN = 8'b0111000;
+
 wire rst_n, ena;
 wire error; 
 wire [7:0] ui_in;
-wire [7:0] hash; 
+wire [7:0] uo_out; 
 wire [7:0] uio_in; 
 wire [7:0] uio_out;
-wire [7:0]  uio_oe;
+wire [7:0] uio_oe;
+wire [SWITCH_W-1:0] switch;
+wire [LED_W-1:0] led;
 
-assign rst_n = ~switch_i[0];
-assign ena = ~switch_i[1];
+assign rst_n = ~switch[0];
+assign ena = ~switch[1];
+assign led[0] = error;
 
-assign ui_in = pmodA_i;
-assign uio_in = { 5'b0, pmodB_io[2:0] & ~uio_oe[2:0]};
+/* IO buffers */ 
+genvar i; 
+generate 
+	for (i=0; i < PMOD_W; i=i+1) begin : g_pmod_io_buff
+		IBUF m_ibuf(
+			.I(pmodA_i[i]),
+			.O(ui_in[i])
+		);
+		OBUF m_obuf(
+			.I(uo_out[i]),
+			.O(pmodC_o[i])
+		);
+		IOBUF m_iobuf (
+			.O(uio_in[i]),
+			.IO(pmodB_io[i]),
+			.I(uio_out[i]),
+			.T(DISSABLE_IO_PIN[i])
+		);
+	end
+	for (i=0; i < SWITCH_W; i=i+1) begin: g_switch_i_buff
+		IBUF m_ibuf(
+			.I(switch_i[i]),
+			.O(switch[i])
+		);
+	end
+	for (i=0; i < LED_W; i=i+1) begin: g_led_o_buff
+		OBUF m_obuf(
+			.I(led[i]),
+			.O(led_o[i])
+		);
+	end
+endgenerate
 
-assign pmodC_o =  hash;
-assign pmodB_io[3] = uio_out[3] & uio_oe[3];
-assign pmodB_io[7] = uio_out[7] & uio_oe[7];
-
-assign led_o = error;
 top m_top(
 	.ui_in(ui_in),
-	.uo_out(hash),
+	.uo_out(uo_out),
 	.uio_in(uio_in),
 	.uio_out(uio_out),
 	.uio_oe(uio_oe),
