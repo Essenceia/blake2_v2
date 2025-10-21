c94af3dd8c9742b61b7ede793948ffca4a23c7cb
Julia Desmazes - ws <julia.desmazes@gmail.com>
2025-10-21 10:35:54 -0700

    updating IO data sync

--- fpga/basys3/emulator.v
+++ fpga/basys3/emulator.v
@@ -5,22 +5,28 @@ module emulator #(
 	parameter LED_W = 1
 )
 (
-	input wire clk_src_i,
+	input wire clk_bus_i, /* 100 MHz */
 
 	input wire [SWITCH_W:0] switch_i,
 
-	input  wire [PMOD_W-1:0]  pmodA_i,  
-	inout  wire [PMOD_W-1:0]  pmodB_io,  
-	output wire [PMOD_W-1:0]  pmodD_o,  
+	// PmodA	
+	input  wire [PMOD_W-1:0]  data_i,  
+	// Pmod B
+	input  wire [2:0]         data_ctrl_i,
+	output wire [1:0]         hash_ctrl_o,
+	// Pmod D
+	output wire [PMOD_W-1:0]  hash_o,  
  
 	output wire [LED_W-1:0] led_o
 );
 localparam [7:0] DISSABLE_IO_PIN = 8'b0111000;
 
-wire clk_ibufg, clk;
-wire rst_n, rst_n_q;
-wire ena, ena_q;
-wire error; 
+wire clk_ibuf, clk_io_bus, clk_pll, clk_pll_feedback, clk;
+wire pll_lock;
+wire rst_async, rst_n_q;
+wire ena_async, ena_q;
+wire error;
+ 
 wire [7:0] ui_in;
 wire [7:0] uo_out; 
 wire [7:0] uio_in; 
@@ -29,49 +35,108 @@ wire [7:0] uio_oe;
 wire [SWITCH_W-1:0] switch;
 wire [LED_W-1:0] led;/* clk */
 
-/* clk */ 
-IBUFG m_ibuf_clk(
+(* IOB = TRUE *) reg [PMOD_W-1:0] data_bus_q, hash_bus_q;
+(* IOB = TRUE *) reg [2:0] data_ctrl_bus;
+(* IOB = TRUE *) reg [1:0] hash_ctrl_bus;
+reg [PMOD_W-1:0] data_q, hash_q;
+reg [2:0] data_bus;
+reg [1:0] hash_bus;
+
+/* BUS */ 
+IBUF m_ibuf_clk(
 	.I(clk_src_i),
-	.O(clk_ibufg)
+	.O(clk_ibuf)
+);
+BUFR m_bufio_clk(
+	.I(clk_ibuf),
+	.O(clk_io_bus)
+);
+BUFG m_bufg_clk(
+	.I(clk_ibuf),
+	.O(clk)
+);
+// I/O bank buffers, driver by bufr, optimized skew with parallel clk
+// infer ILOGIC and OLOGIC blocks
+always @(posedge clk_io_bus) begin
+	data_bus_q <= data_i;
+	data_ctrl_bus_q <= data_ctrl_i;
+	hash_bus_q <= hash_q;
+	hash_ctrl_bus_q <= hash_ctrl_q;
+end
+assign hash_o = hash_bus_q;
+assign hash_ctr_o = hash_ctrl_bus_q;
+
+// sync to global clock
+always @(posedge clk) begin
+	data_q <= data_bus_q;
+	data_ctrl_q <= data_ctrl_bus_q;
+	hash_q <= hash;
+	hash_ctrl_q <= hash_ctrl;
+end
+
+// Global clock based on bus clock, using the same frequency
+// using the inherent jitter filtering capability of the PLL
+// and phase locked on bus clokc
+PLLE2_BASE #(
+   .CLKFBOUT_MULT(2),        
+   .CLKIN1_PERIOD(10.0),      
+   .CLKOUT0_DIVIDE(2)
+) m_global_clk_pll (
+   .CLKFBIN(clk_pll_feedback)
+   .CLKFBOUT(clk_pll_feedback),
+   .CLKIN1(clk_io_bus),    
+   .CLKOUT0(clk_pll),
+   .CLKOUT1(),
+   .CLKOUT2(),
+   .CLKOUT3(),
+   .CLKOUT4(),
+   .CLKOUT5(),
+   .LOCKED(pll_lock),
+   .PWRDWN(1'b0),
+   .RST(rst_async) 
 );
 
-/* IO buffers */ 
-genvar i; 
+genvar i;
 generate 
-	for (i=0; i < PMOD_W; i=i+1) begin : g_pmod_io_buff
-		IBUF m_ibuf(
-			.I(pmodA_i[i]),
-			.O(ui_in[i])
-		);
-		OBUF m_obuf(
-			.I(uo_out[i]),
-			.O(pmodC_o[i])
-		);
-		IOBUF m_iobuf (
-			.O(uio_in[i]),
-			.IO(pmodB_io[i]),
-			.I(uio_out[i]),
-			.T(DISSABLE_IO_PIN[i])
-		);
-	end
-	for (i=0; i < SWITCH_W; i=i+1) begin: g_switch_i_buff
-		IBUF m_ibuf(
+	for (i=0; i < SWITCH_W; i++) begin: g_switch_ibuf
+		IBUF m_switch_ibuf(
 			.I(switch_i[i]),
 			.O(switch[i])
 		);
 	end
-	for (i=0; i < LED_W; i=i+1) begin: g_led_o_buff
-		OBUF m_obuf(
-			.I(led[i]),
-			.O(led_o[i])
+	for (i=0; i < LED_W; i=i+1) begin: g_led_obuf
+		OBUF m_led_obuf(
+			.I(led),
+			.O(led_o)
 		);
 	end
 endgenerate
-
-assign rst_n = ~switch[0];
+assign rst_async = switch[0];
 assign ena = ~switch[1];
 
-assign led[0] = error;
+assign led[0] = rst_async;
+assign led[1] = pll_lock;
+assign led[2] = error_q;
+
+/* rst */
+always @(posedge clk or posedge rst_async) 
+	if (rst_async)
+		rst_n_q <= 1'b1;
+	else
+		rst_n_q <= ~pll_lock; 
+end
+
+debounce m_switch_debounce(
+	.clk(clk),
+	.rst_async(rst_async),
+	.switch_i(switch_i[i]),
+	.switch_o(ena)
+);
+
+assign ui_in = data_q;
+assign uio_in = {5'b0, data_ctrl_q);
+assign hash = uo_out;
+assign hash_ctrl = {uio_out[7], uio_out[3]};
 
 top m_top(
 	.ui_in(ui_in),
@@ -81,13 +146,13 @@ top m_top(
 	.uio_oe(uio_oe),
 	.ena(ena),
 	.clk(clk),
-	.rst_n(rst_n)
+	.rst_n(rst_n_q)
 );
 
 // error 
 error m_err(
 	.clk(clk), 
-	.nreset(rst_n), 
+	.nreset(rst_n_q), 
 	
 	.uio_oe(uio_oe),
 	.uio_out(uio_out),
---
+++ fpga/debounce.v
@@ -0,0 +1,50 @@
+module debounce  #(
+    parameter W=1, 
+    parameter N=3, 
+    parameter CNT_W=12
+)(
+    input wire clk,
+    input wire rst_async,
+    input wire [W-1:0] switch_i,
+    output wire [W-1:0] switch_o
+);
+
+reg [CNT_W-1:0] cnt_reg;
+reg [N-1:0] debounce_reg[W-1:0];
+reg [W-1:0] state;
+integer k;
+
+/* sample the switch status at a defined time interval and
+ * ouput new value if sampled state is stable over N samples */
+always @(posedge clk or posedge rst_async) begin
+    if (rst_async) begin
+        cnt_reg <= {CNT_W{1'b0}};
+        state <= 0;
+        
+        for (k = 0; k < W; k = k + 1) begin
+            debounce_reg[k] <= 0;
+        end
+    end else begin
+        cnt_reg <= cnt_reg + d1;
+        
+        if (cnt_reg == {CNT_W{1'b0}}) begin
+            for (k = 0; k < W; k = k + 1) begin
+                debounce_reg[k] <= {debounce_reg[k][N-2:0], switch_i[k]};
+            end
+        end
+        
+        for (k = 0; k < W; k = k + 1) begin
+            if (|debounce_reg[k] == 0) begin
+                state[k] <= 0;
+            end else if (&debounce_reg[k] == 1) begin
+                state[k] <= 1;
+            end else begin
+                state[k] <= state[k];
+            end
+        end
+    end
+end
+assign switch_o = state;
+
+endmodule
+
