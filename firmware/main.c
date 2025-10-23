5cdfbc97292b47cc26e42d81a5f1ea32b1d3fd48
Julia Desmazes <julia.desmazes@gmail.com>
2025-10-23 14:28:43 -0500

    FPGA aquired lock on pio bus clk

--- firmware/main.c
+++ firmware/main.c
@@ -12,7 +12,8 @@
 #define PIO_CLK 1
 #define macro_str(x) #x
 
-#define BUS_CLK_FREQ_HZ 40000000 // 40 MHz
+#define PICO_SYS_CLK_HW 200000000 // 200 MHz
+#define BUS_CLK_FREQ_HZ (float) 40000000.0 // 40 MHz
 
 #define log_init(PIO_IDX) printf(#PIO_IDX " init sucess %d:{%d:%d}", s, sm[PIO_IDX], offset[PIO_IDX]);
 int main() {
@@ -21,6 +22,9 @@ int main() {
 	uint offset[PIO_N];
 	float clk_div; 
 	uint led = 1;
+
+	// set system clk
+	set_sys_clock_hz(PICO_SYS_CLK_HW, true);
 	
 	stdio_init_all();
 	
@@ -40,15 +44,19 @@ int main() {
 		BUS_CLK_PIN, 1, true);
 	log_init(PIO_CLK);
 	hard_assert(s);
-	clk_div = (float)clock_get_hz(clk_sys) / BUS_CLK_FREQ_HZ; 
-	bus_clk_program_init(pio[PIO_CLK], sm[PIO_CLK], offset[PIO_CLK], BUS_CLK_PIN, clk_div);	
+	clk_div = (float)clock_get_hz(clk_sys) / (BUS_CLK_FREQ_HZ*2); 
+	bus_clk_program_init(pio[PIO_CLK], sm[PIO_CLK], offset[PIO_CLK], BUS_CLK_PIN, clk_div);
 
+	gpio_set_drive_strength(BUS_CLK_PIN, GPIO_DRIVE_STRENGTH_12MA);
+	gpio_set_slew_rate(BUS_CLK_PIN, GPIO_SLEW_RATE_FAST);
+
+	pio_sm_set_enabled(pio[PIO_CLK], sm[PIO_CLK], true); 
 	
 
     while (true) {
 		pio_sm_put_blocking(pio[PIO_LED], sm[PIO_LED], led);
 		led = led ? 0:1;
-		printf("Teapot ! <3\n");
+		printf("Teapot ! div %f <3\n", clk_div);
         sleep_ms(LED_DELAY_MS);
     }
 }
