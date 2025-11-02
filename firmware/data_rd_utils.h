8c9ada72bc467ab10f7b40edd5c75cd0aeb49cfc
Julia Desmazes <julia.desmazes@gmail.com>
2025-11-01 20:28:45 -0500

    adding hash read utils

---
+++ firmware/data_rd_utils.h
@@ -0,0 +1,17 @@
+#ifndef _DATA_RD_UTILS
+#define _DATA_RD_UTILS
+
+#include "pico/stdlib.h"
+#include "hardware/pio.h"
+
+// init dma
+uint init_rd_dma_channel(PIO pio, uint sm);
+
+// setup dma stream to capture hash result
+void setup_rd_dma_hash_stream(uint dma_chan, uint kk);
+
+// read hash from already setup read stream 
+void read_hash(uint8_t* hash, size_t *hl,uint8_t kk, uint dma_chan);
+
+#endif // _DATA_RD_UTILS
+
